c4ec9bd79e2e4c8e92e96cabed4048dd3e5799e3
João Pedro Mendes dos Santos <66846607+Joaosam@users.noreply.github.com>
2022-04-29 22:47:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function displayPassword() {
+  var password = document.getElementById('password')
+  if (password.type == 'password') {
+    password.type = 'text'
+    password.style.backgroundImage = "url('images/lock-focus.svg')"
+    password.style.backgroundRepeat = 'no-repeat'
+    password.style.backgroundPosition = '5px'
+    password.style.paddingLeft = '30px'
+    password.style.border = '2px solid #ffc632'
+    password.style.outline = 'none'
+  } else {
+    password.type = 'password'
+    password.style.backgroundImage = ''
+    password.style.backgroundPosition = '5px'
+    password.style.paddingLeft = '30px'
+    password.style.border = ''
+    password.style.outline = 'none'
+  }
+}
+
+function entrar() {
+  window.open('window.html')
+}
